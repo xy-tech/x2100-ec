9ccdeff97af319b421a1fe5467c8a52cc866ccd7
Joshua Wise <joshua@joshuawise.com>
2020-08-28 22:44:54 -0700

    Port LCD brightness change.

--- menu.sh
+++ menu.sh
@@ -2,6 +2,7 @@
 set -x
 dialog --checklist 'Choose the desired patches' 0 0 0 \
 	fix-ec-debug            'Allow hot-patching of EC (incomplete).' on \
+	lcd-brightness		'Allow lowering LCD backlight brightness to 1%'		on \
 	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"

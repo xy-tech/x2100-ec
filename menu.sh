5e4db27613f49ccafacab4bbaa241f51aafd9855
Joshua Wise <joshua@joshuawise.com>
2020-08-28 21:38:30 -0700

    Initial hotpatch patch.

--- menu.sh
+++ menu.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -x
 dialog --checklist 'Choose the desired patches' 0 0 0 \
-	fn-swap			'Swap Fn and Ctrl keys'					off \
-	lcd-brightness		'Allow lowering LCD backlight brightness to 1%'		on \
-	lcd-backlight-925hz	'Increase the LCD backlight PWM frequency to ~925Hz'	on \
-	fan-72deg		'Make fan blow at 100% speed on 72+°C CPU temperature'	on \
-	enable-hotkeys		'Generate scancodes for all hotkeys'			on \
+	fix-ec-debug            'Allow hot-patching of EC (incomplete).' on \
 	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"

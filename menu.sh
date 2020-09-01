e622ac40d9bee80f06e6b9352e69cce931333f08
Joshua Wise <joshua@joshuawise.com>
2020-08-31 23:57:09 -0700

    Add some notes.

--- menu.sh
+++ menu.sh
@@ -6,7 +6,7 @@ dialog --checklist 'Choose the desired patches' 0 0 0 \
 	fn-swap			'Swap Fn and Ctrl keys (untested, but should work)' off \
 	lcd-backlight-925hz     'LCD backlight to 925Hz (untested, but should work)' off \
 	true-battery-reading	'Fix battery reading above 70% (experimental)' on \
-	battery-current		'Dump battery current to 0x10b00 in EC ram (SUPER EXPERIMENTAL)' off \
+	battery-current		'Fix battery current measurement (SUPER EXPERIMENTAL)' off \
 	2> selected
 for p in $(cat selected); do
 	fn="patches/$p.rapatch"
